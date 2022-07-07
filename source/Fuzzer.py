1fc3268ed1b70bcfe64cb11664195174319f9cae
jayden <jayden.leung128@gmail.com>
2022-07-07 04:59:42 -0400

    updated harness and chmod binaries

--- source/Fuzzer.py
+++ source/Fuzzer.py
@@ -1,17 +1,25 @@
-from source.PDFFuzzer import PDFFuzzer
+from PDFFuzzer import PDFFuzzer
 from sys import argv
 
 def main():
-    # Read input
-    if len(argv) != 2:
-        print("Usage: %s <pdf_file>" % argv[0])
+
+    if len(argv) != 3:
+        print("Usage: %s <binary> <binaryinput.txt>" % argv[0])
         exit(1)
 
-    fuzzer = PDFFuzzer()
+    binary_path = argv[1]
+    binary_input_path = argv[2]
+
+    print("Fuzzing this thing...")
+
+    if PDFFuzzer.isType(binary_input_path):
+        fuzzer = PDFFuzzer(binary_path, binary_input_path)
+    
+    
     result = fuzzer.fuzz()
-    print(result)
-
-
+    print("Found bad input, writing to bad.txt")
+    with open("bad.txt", "w") as f:
+        f.write(result)
 
 
 if __name__ == "__main__":
--- source/PDFFuzzer.py
+++ source/PDFFuzzer.py
@@ -1,15 +1,26 @@
 class PDFFuzzer:
-    def __init__(self):
-        print("PDFFuzzer")
+    def __init__(self, binary_path, binary_input_path):
+        print("Running PDF Fuzzer")
+        self.binary_path = binary_path
+        self.binary_input_path = binary_input_path
+        print(f"binary_path: {self.binary_path}")
+        print(f"binary_input_path: {self.binary_input_path}")
         pass
 
+    @staticmethod
+    def isType(binary_path):
+        """_summary_
+        Check if the type is a pdf
+        """
+
+        return True
 
     def fuzz(self):
         """_summary_
         Fuzz the PDF file
 
         _returns_
-        Returns a list of input that crashed the binary
+        Returns the input that crashed the binary
         """
 
-        return ["user input"]
+        return "blah"
