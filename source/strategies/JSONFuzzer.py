9aab4aab3389d43d0a0d3f0d27ac50f9b37a7414
Cameron Huang <cameronhuang96@gmail.com>
2022-07-15 13:03:41 +1000

    added integer mutator

--- source/strategies/JSONFuzzer.py
+++ source/strategies/JSONFuzzer.py
@@ -9,6 +9,14 @@ class JSONFuzzer(Fuzzer):
         pass
 
     def fuzz(self):
+        # Open the input JSON file for reading
+        f = open(self.binary_input_path, "r")
+        # Create a dictionary from the input JSON file's contents
+        input_file_dict = json.load(f)
+        # Iterate through the dictionary, seeing if changing 
+        for value in input_file_dict.values():
+              print(value)
+
         return "blah"
 
     def get_coverage():
--- source/strategies/mutators/IntegerMutator.py
+++ source/strategies/mutators/IntegerMutator.py
@@ -1,8 +1,27 @@
 from TypeMutator import TypeMutator
+import sys
 class IntegerMutator(TypeMutator):
     def __init__(self):
         pass
     
     @staticmethod
-    def integer_bit_flip():
-        return "blah"
+    def bit_flip(integer):
+        return ~integer
+    
+    @staticmethod
+    def make_negative(integer):
+        return -integer
+    
+    @staticmethod
+    def make_huge(integer):
+        return integer ^ sys.maxsize #change
+    
+    @staticmethod
+    def make_float(integer):
+        return float(integer)
+    
+    @staticmethod
+    def make_bool(integer):
+        return bool(integer)
+
+    
