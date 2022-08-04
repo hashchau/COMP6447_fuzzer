4e8c1a320b7f9209843eec0179616f019c13b286
CyrusWilkie <cyrus.wilkie@gmail.com>
2022-08-04 05:21:34 -0400

    Plaintext2 progress

--- source/strategies/PlaintextMutator.py
+++ source/strategies/PlaintextMutator.py
@@ -9,39 +9,45 @@ class PlaintextMutator(FormatMutator):
 
     @staticmethod
     def mutate_once(payload):
-        mutated_payload = PlaintextMutator.mutate_string(payload)
-
+        mutated_payload = ""
+        
+        inputs = payload.split('\n')[:-1]
+        
+        for line in inputs:
+            mutated_line = PlaintextMutator.mutate_string(line)
+            mutated_payload = mutated_payload + mutated_line + '\n'
+        
         return [mutated_payload]
 
     @staticmethod
     def mutate_all(payload):
-        mutated_payload = PlaintextMutator.mutate_string(payload)
+        mutated_payload = ""
+        
+        inputs = payload.split('\n')[:-1]
 
+        for line in inputs:
+            mutated_line = PlaintextMutator.mutate_string(line)
+            mutated_payload = mutated_payload + mutated_line + '\n'
+        
         return [mutated_payload]
 
     @staticmethod
     def mutate_string(payload):
-        num_mutations = random.randint(1,10)
+        num_mutations = random.randint(1,4)
         mutated_string = payload
-
+        
         for i in range(num_mutations):
-            mutation = random.randint(0,7)
+            mutation = random.randint(0,4)
 
             if mutation == 0:
                 mutated_string = StringMutator.delete_char(mutated_string)
             elif mutation == 1:
-                mutated_string = StringMutator.flip_bit(mutated_string)
-            elif mutation == 2:
-                mutated_string = StringMutator.insert_new_line(mutated_string)
-            elif mutation == 3:
-                mutated_string = StringMutator.insert_new_line_with_delimiter(mutated_string)
-            elif mutation == 4:
                 mutated_string = StringMutator.insert_format_string(mutated_string)
-            elif mutation == 5:
+            elif mutation == 2:
                 mutated_string = StringMutator.extend_string(mutated_string)
-            elif mutation == 6:
+            elif mutation == 3:
                 mutated_string = StringMutator.repeat_string(mutated_string)
-            elif mutation == 7:
+            elif mutation == 4:
                 mutated_string = StringMutator.random_chars(mutated_string)
             
 
--- source/strategies/mutators/StringMutator.py
+++ source/strategies/mutators/StringMutator.py
@@ -35,6 +35,7 @@ class StringMutator(TypeMutator):
     @staticmethod
     def insert_format_string(input_string):
         format_chars = ['%s', '%n', '%x', '%p']
+        print(len(input_string))
         num_replacements = random.randint(0, len(input_string) - 1)
         mutated_string = input_string
         
@@ -47,25 +48,28 @@ class StringMutator(TypeMutator):
 
     @staticmethod
     def extend_string(input_string):
-        extend_size = random.randint(0, 100000)
+        extend_size = random.randint(0, 100)
         mutated_string = input_string + ('A'*extend_size)
         return mutated_string
 
     @staticmethod
     def repeat_string(input_string):
-        repeats = random.randint(0, 1000)
+        repeats = random.randint(0, 5)
         mutated_string = input_string
         mutated_string = mutated_string*repeats
         return mutated_string
 
     @staticmethod
     def random_chars(input_string):
+        print(len(input_string))
         num_replacements = random.randint(0, len(input_string) - 1)
         mutated_string = input_string
         for i in range(0, num_replacements):
             replacement_location = random.randint(0, len(input_string) - 1)
             replacement_character = chr(random.randint(0, 255))
-            mutated_string = mutated_string[:replacement_location] + replacement_character + mutated_string[replacement_location + 1:]
+            
+            if replacement_character != '\n':
+                mutated_string = mutated_string[:replacement_location] + replacement_character + mutated_string[replacement_location + 1:]
         
         return mutated_string
     
